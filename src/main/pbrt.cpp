6b2ec71e2aacceb72a3fa423ae33150524387233
miccall <316459829@qq.com>
2020-02-13 10:52:59 +0800

    PBRT init

--- src/main/pbrt.cpp
+++ src/main/pbrt.cpp
@@ -1,12 +1,17 @@
 ﻿// miccall_PBRT.cpp: 定义应用程序的入口点。
 //
 
-#include "miccall_PBRT.h"
+#include "pbrt.h"
+#include <glog/logging.h>
 
 using namespace std;
+using namespace pbrt;
 
-int main()
+int main(int argc, char* argv[])
 {
+	google::InitGoogleLogging(argv[0]);
+	char a;
 	cout << "Hello CMake." << endl;
+	cin.get(a);
 	return 0;
 }
